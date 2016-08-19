ee5f5d8a1cefcc7462b809d1e644d4bb37e2af57
rummsi <rummsi@04f4a7f7-fcc1-4ebe-bf4c-d10fe30f76e9>
2016-08-19 18:36:30 +0000

    - Projecto 2 Moons (versão 1.7.3).

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+//### Database access ###//
+
+$database					= array();
+$database['host']			= 'localhost';
+$database['port']			= '3306';
+$database['user']			= 'ptgamespt';
+$database['userpw']			= 'ptgamespt';
+$database['databasename']	= 'ptgamespt';
+$database['tableprefix']	= 'moons_';
+$salt						= 'W9YD7AUtj.3gbeKfSn1Eiw'; // 22 digits from the alphabet "./0-9A-Za-z"
+
+//### Do not change beyond here ###//
+?>
