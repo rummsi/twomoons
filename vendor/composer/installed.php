b3b00b08e5bd0ea098e0684a0816cf1f8b74eefc
Rui Silva <ptgamespontopt@gmail.com>
2022-12-24 10:21:09 +0000

    - implementação autoloader + composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/new-star',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '16f41078b1d594f6a1286dc56c657540c73b5b98',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/new-star' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '16f41078b1d594f6a1286dc56c657540c73b5b98',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
